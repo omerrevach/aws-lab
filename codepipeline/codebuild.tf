4413d4dae5f1d573eb2a238bce3d7f0e93321a5d
omerrevach <hairviction@gmail.com>
2025-03-26 14:33:10 +0200

    fixed image

--- codepipeline/codebuild.tf
+++ codepipeline/codebuild.tf
@@ -1,47 +1,48 @@
 resource "aws_codebuild_project" "lab_app_build" {
-  name          = "lab-docker-pipeline"
-  service_role  = aws_iam_role.codebuild_role.arn
+  name = "lab-docker-pipeline"
+  service_role = aws_iam_role.codebuild_role.arn
+  
   artifacts {
     type = "NO_ARTIFACTS"
   }
-
+  
   environment {
-    compute_type                = "BUILD_GENERAL1_SMALL"
-    image                       = "aws/codebuild/standard:6.0"
-    type                        = "LINUX_CONTAINER"
-    privileged_mode             = true
-
+    compute_type = "BUILD_GENERAL1_SMALL"
+    image = "aws/codebuild/amazonlinux2-x86_64-standard:4.0"  # This image has Docker pre-installed
+    type = "LINUX_CONTAINER"
+    privileged_mode = true
+    
     environment_variable {
-      name  = "DOCKERHUB_USERNAME"
+      name = "DOCKERHUB_USERNAME"
       value = var.dockerhub_username
     }
-
+    
     environment_variable {
-      name  = "DOCKERHUB_PASSWORD"
+      name = "DOCKERHUB_PASSWORD"
       value = data.aws_secretsmanager_secret.dockerhub_password.arn
-      type  = "SECRETS_MANAGER"
+      type = "SECRETS_MANAGER"
     }
-
+    
     environment_variable {
-      name  = "AWS_REGION"
+      name = "AWS_REGION"
       value = var.aws_region
     }
-
+    
     environment_variable {
-      name  = "AWS_ACCOUNT_ID"
+      name = "AWS_ACCOUNT_ID"
       value = data.aws_caller_identity.current.account_id
     }
   }
-
+  
   source {
-    type            = "GITHUB"
-    location        = "https://github.com/omerrevach/aws-lab"
+    type = "GITHUB"
+    location = "https://github.com/omerrevach/aws-lab"
     git_clone_depth = 1
-    buildspec       = "buildspec.yml"
+    buildspec = "buildspec.yml"
   }
-
+  
   source_version = "lab-pipeline"
-
+  
   tags = {
     Name = "lab-docker-build"
   }
--- codepipeline/iam.tf
+++ codepipeline/iam.tf
@@ -16,13 +16,32 @@ resource "aws_iam_policy" "codebuild_policy" {
     Version = "2012-10-17",
     Statement = [
       {
-        Effect   = "Allow",
-        Action   = ["logs:*", "codebuild:*", "s3:*"],
+        Effect = "Allow",
+        Action = [
+          "logs:CreateLogGroup",
+          "logs:CreateLogStream",
+          "logs:PutLogEvents"
+        ],
         Resource = "*"
       },
       {
-        Effect   = "Allow",
-        Action   = ["secretsmanager:GetSecretValue"],
+        Effect = "Allow",
+        Action = [
+          "s3:GetObject",
+          "s3:GetObjectVersion",
+          "s3:PutObject",
+          "s3:ListBucket"
+        ],
+        Resource = "*"
+      },
+      {
+        Effect = "Allow",
+        Action = ["secretsmanager:GetSecretValue"],
+        Resource = "*"
+      },
+      {
+        Effect = "Allow",
+        Action = ["codebuild:*"],
         Resource = "*"
       }
     ]
@@ -30,7 +49,7 @@ resource "aws_iam_policy" "codebuild_policy" {
 }
 
 resource "aws_iam_role_policy_attachment" "codebuild_attach" {
-  role       = aws_iam_role.codebuild_role.name
+  role = aws_iam_role.codebuild_role.name
   policy_arn = aws_iam_policy.codebuild_policy.arn
 }
 
@@ -52,18 +71,39 @@ resource "aws_iam_policy" "codepipeline_policy" {
     Version = "2012-10-17",
     Statement = [
       {
-        Effect   = "Allow",
-        Action   = ["codebuild:StartBuild"],
+        Effect = "Allow",
+        Action = [
+          "codebuild:BatchGetBuilds",
+          "codebuild:StartBuild",
+          "codebuild:BatchGetBuildBatches",
+          "codebuild:StartBuildBatch"
+        ],
         Resource = "*"
       },
       {
-        Effect   = "Allow",
-        Action   = ["s3:*"],
+        Effect = "Allow",
+        Action = [
+          "s3:GetObject",
+          "s3:GetObjectVersion",
+          "s3:GetBucketVersioning",
+          "s3:PutObjectAcl",
+          "s3:PutObject",
+          "s3:ListBucket"
+        ],
         Resource = "*"
       },
       {
-        Effect   = "Allow",
-        Action   = ["codepipeline:*"],
+        Effect = "Allow",
+        Action = [
+          "codestar-connections:UseConnection"
+        ],
+        Resource = "*"
+      },
+      {
+        Effect = "Allow",
+        Action = [
+          "codepipeline:*"
+        ],
         Resource = "*"
       }
     ]
@@ -71,6 +111,6 @@ resource "aws_iam_policy" "codepipeline_policy" {
 }
 
 resource "aws_iam_role_policy_attachment" "codepipeline_attach" {
-  role       = aws_iam_role.codepipeline_role.name
+  role = aws_iam_role.codepipeline_role.name
   policy_arn = aws_iam_policy.codepipeline_policy.arn
 }
