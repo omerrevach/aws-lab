13106f6ddd44dd2ab2444da57352e1f5c0597480
Omer Revach <hairviction@gmail.com>
2025-03-27 15:41:49 +0200

    Delete tf directory

--- tf/data.tf
+++
@@ -1,4 +0,0 @@
-data "aws_availability_zones" "available" {}
-data "aws_eks_cluster_auth" "cluster" {
-  name = module.eks.cluster_name
-}
--- tf/modules/eks/vars.tf
+++
@@ -1,8 +0,0 @@
-variable "cluster_name" {}
-variable "vpc_id" {}
-variable "private_subnets" {
-  type        = list(string)
-}
-variable "eks_fargate_pod_execution_role_arn" {}
-variable "ec2_ssm_role_arn" {}
-
