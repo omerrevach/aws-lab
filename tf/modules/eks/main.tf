94099d30fcfd82d358bb2318de1b13eb84ca426b
Omer Revach <hairviction@gmail.com>
2025-03-28 10:23:57 +0300

    Update readme.md

--- tf/modules/eks/main.tf
+++ tf/modules/eks/main.tf
@@ -11,8 +11,8 @@ module "eks" {
 
   vpc_id                  = var.vpc_id
   subnet_ids              = var.private_subnets
-  cluster_endpoint_public_access  = true
-  cluster_endpoint_private_access = false
+  cluster_endpoint_public_access  = false
+  cluster_endpoint_private_access = true
 
   enable_cluster_creator_admin_permissions = true
 
